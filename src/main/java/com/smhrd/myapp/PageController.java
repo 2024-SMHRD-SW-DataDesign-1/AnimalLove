f1160a77b4a7715ee128e1d01c56f953114de8f7
Bumilord <psb1994@naver.com>
2024-06-19 12:18:18 +0900

    헤더 매칭버튼 눌렀을때 로그인안했으면 로그인페이지로 로그인했으면 동물등록페이지로 이동하게 수정 성범 커밋

--- src/main/java/com/smhrd/myapp/PageController.java
+++ src/main/java/com/smhrd/myapp/PageController.java
@@ -1,6 +1,7 @@
 package com.smhrd.myapp;
 
 import javax.print.attribute.SetOfIntegerSyntax;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -10,53 +11,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.smhrd.myapp.model.Animal;
+import com.smhrd.myapp.model.MavenMember;
 
 @Controller
 public class PageController {
 	// index 페이지를 리턴해주는 메서드
-	
-	@RequestMapping(value="/index", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index() {
 		// view resolver : /WEB-INF/views/index.jsp
 		return "index";
 	}
-	
-	@RequestMapping(value="/join", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public String join() {
 		return "page/join";
 	}
-	
-	@RequestMapping(value="/login", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "page/login";
 	}
-	
-	@RequestMapping(value="/update", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public String update() {
 		return "page/update";
 	}
-	
+
 //	@RequestMapping(value="/chat", method=RequestMethod.GET)
 //	public String chat() {
 //		return "chatting";
 //		// return "chatting" => chatting.jsp (WEB-INF/spring/appServlet/servlet-context.xml 에서 설정)
 //	}
-	@RequestMapping(value="/common/header", method=RequestMethod.GET)
+	@RequestMapping(value = "/common/header", method = RequestMethod.GET)
 	public String header() {
 		return "common/header";
 	}
-	
-	@RequestMapping(value="/location", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/location", method = RequestMethod.GET)
 	public String location() {
 		return "location";
 	}
-	
-	@RequestMapping(value="/map", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/map", method = RequestMethod.GET)
 	public String map() {
 		return "common/map/cookie";
 	}
-	
-	@RequestMapping(value="/information", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/information", method = RequestMethod.GET)
 	public String information() {
 		return "page/information";
 	}
@@ -64,65 +66,70 @@ public class PageController {
 //	public String list() {
 //		return "member/list";
 //	}
-	
-	@RequestMapping(value="/chatting/{c_id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/chatting/{c_id}", method = RequestMethod.GET)
 	public String chatting(@PathVariable("c_id") String c_id, Model model) {
 		model.addAttribute("c_id", c_id);
 		return "chatting";
 	}
-	
-	@RequestMapping(value="/chatList", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/chatList", method = RequestMethod.GET)
 	public String chatList() {
 		return "chatList";
 	}
-	
+
 	// 채팅방리스트 화면
-	@RequestMapping(value="/mainchatList", method=RequestMethod.GET)
+	@RequestMapping(value = "/mainchatList", method = RequestMethod.GET)
 	public String mainchatList() {
 		return "common/chat/chatlist";
 	}
-	
-	//메인 화면
-	@RequestMapping(value="/", method=RequestMethod.GET)
+
+	// 메인 화면
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String main() {
 		// view resolver : /WEB-INF/views/index.jsp
 		return "page/main";
 	}
-	
-	
+
 	// 매칭화면
-	@RequestMapping(value="/matching", method=RequestMethod.GET)
-	public String animalInfo( ) {
-	// view resolver : /WEB-INF/views/index.jsp
-	return "page/animalInfo";
+	@RequestMapping(value = "/matching", method = RequestMethod.GET)
+	public String animalInfo(HttpServletRequest request) {
+		// 로그인 세션 가져오기
+		HttpSession session = request.getSession();
+		// 로그인 세션에서 값 가져오기 --> 로그인 당시 정보
+		MavenMember member2 = (MavenMember) session.getAttribute("member");
+		if (member2 != null) { // 로그인 상태
+			return "page/animalInfo";
+		} else { // 로그인 안한상태
+			return "page/login";
+		}
+		// view resolver : /WEB-INF/views/index.jsp
+
 	}
-	
-	//메인 화면
-	@RequestMapping(value="/matching_pic", method=RequestMethod.GET)
-	public String animalInfo2( Animal animal, HttpSession session) {
+
+	// 매칭화면2
+	@RequestMapping(value = "/matching_pic", method = RequestMethod.GET)
+	public String animalInfo2(Animal animal, HttpSession session) {
 		// view resolver : /WEB-INF/views/index.jsp
 		session.setAttribute("animal", animal);
-		Animal tem =  (Animal)session.getAttribute("animal");
+		Animal tem = (Animal) session.getAttribute("animal");
 
 		return "page/animalInfo2";
 	}
-	
-	//ID찾기
-		@RequestMapping(value="/findid", method=RequestMethod.GET)
-		public String findId() {
-			// view resolver : /WEB-INF/views/index.jsp
-			return "page/findId";
-		}
-	
+
+	// ID찾기
+	@RequestMapping(value = "/findid", method = RequestMethod.GET)
+	public String findId() {
+		// view resolver : /WEB-INF/views/index.jsp
+		return "page/findId";
+	}
+
 	// 동물정보 등록
-	@RequestMapping(value="/animal_info/save", method=RequestMethod.GET)
+	@RequestMapping(value = "/animal_info/save", method = RequestMethod.GET)
 	public String infoSave(Animal animal) {
 		// view resolver : /WEB-INF/views/index.jsp
 
-		return "page/findId";
+		return "page/main";
 	}
-		
-	
-	
-}
 
+}
